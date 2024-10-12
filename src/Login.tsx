0d58d5706c0c4b1c9f55fd53d8423c913f038ad6
simonkadili47 <simonkadili47@gmail.com>
2024-10-12 11:57:58 -0700

    logo

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import LOGO from './assets/LOGO-2-final.png';
+import LOGO from './assets/logo.png';
 
 const Login: React.FC = () => {
   return (
